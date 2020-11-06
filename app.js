3db666cc55baff54ad6e1a7fd42eed54dfb952ae
tpreston96 <tpreston96@gmail.com>
2020-11-06 16:54:29 -0600

    framework and Friday endpoint

---
+++ app.js
@@ -0,0 +1,36 @@
+// add prevGuesses array
+// set up 'prompt'
+// create while loop
+// alert -- tooHigh || tooLow
+
+
+const game = {
+    title: 'Guess the Number!',
+    biggestNum: 100,
+    smallestNum: 1,
+    secretNum: null,
+    play: function() {
+      this.secretNum = Math.floor(Math.random() * 
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+    }
+  };
+  function getGuess(){
+    game.play();
+    let num= this.secretNum;
+    let input = 1;
+    console.log(`input: ${input},secret number: ${num}`);
+    return compGuess(input, num);
+  }
+function compGuess(input, num){
+    while(input != num){
+        if(input < num){
+            console.log(`${input} was too low`);
+        }
+        else if(input > num){
+            console.log(`${input} was too high`);
+        }
+        console.log(num);
+        return input;
+    };
+};
+console.log(`${getGuess()} was not correct!`);
