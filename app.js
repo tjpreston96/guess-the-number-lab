8905e687b546b29c86f037fbf0d7a996fa841da2
tpreston96 <tpreston96@gmail.com>
2020-11-07 14:47:19 -0600

    added css document

--- app.js
+++ app.js
@@ -9,28 +9,51 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
+    prevGuesses: [],
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
     }
   };
-  function getGuess(){
-    game.play();
-    let num= this.secretNum;
-    let input = 1;
-    console.log(`input: ${input},secret number: ${num}`);
-    return compGuess(input, num);
-  }
-function compGuess(input, num){
-    while(input != num){
-        if(input < num){
-            console.log(`${input} was too low`);
-        }
-        else if(input > num){
-            console.log(`${input} was too high`);
-        }
-        console.log(num);
-        return input;
-    };
-};
-console.log(`${getGuess()} was not correct!`);
+
+// // prev guesses propety -> game object -> initialized into empty array
+
+
+function getGuess(){
+    prompt (``)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   function getGuess(){
+//     game.play();
+//     let num= this.secretNum;
+//     let input = 1;
+//     console.log(`input: ${input},secret number: ${num}`);
+//     return compGuess(input, num);
+//   }
+// function compGuess(input, num){
+//     while(input != num){
+//         if(input < num){
+//             console.log(`${input} was too low`);
+//         }
+//         else if(input > num){
+//             console.log(`${input} was too high`);
+//         } 
+//         console.log(num);
+//         return input;
+//     };
+// };
+// console.log(`${getGuess()} was not correct!`);
